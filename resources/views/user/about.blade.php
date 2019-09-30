4d904ee6d6e6e759b51019a90a385faa1ab0d213
[KTharmi] <[tharmikulendiran5@gmail.com]>
2019-09-30 08:38:20 +0530

    about information

--- resources/views/user/about.blade.php
+++ resources/views/user/about.blade.php
@@ -5,6 +5,13 @@
       <img src="{{asset('user/img/builder/builder-vc.jpg')}}" alt=""> 
       <br>
       <br>  
-      <p>We are a developing construction company with emerging and talented employees to offer you the quality buildings</p>
+      <p>Best service will be provided for houses for contract basis, shops, temple construction works, modern houses and drawings for buildings, electricity connections, pipe lining for building water supply, electric repair works for buildings, suveying works, evaluation reports for buildings</p>
+      <p >⚫Houses for contract basis,shops, temple construction works, modern houses </p>
+      <p>⚫Drawings for buildings </p>
+      <p>⚫Electricity connections</p>
+      <p>⚫ Pipe lining for water supply of buildings </p>
+      <p>⚫Electric repair works for buildings</p>
+      <p>⚫Surveying works </p>
+      <p>⚫Evaluation reports for buildings</p>
 </div>
 @endsection 
