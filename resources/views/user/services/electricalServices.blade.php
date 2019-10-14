2fe2281eeb99727747eda12b0707c50e825665bb
rammiahj <rammiahj@gmail.com>
2019-10-14 09:36:14 +0530

    Services

--- resources/views/user/services/electricalServices.blade.php
+++ resources/views/user/services/electricalServices.blade.php
@@ -1,5 +1,17 @@
 @extends('user/app')
 @section('main-contend')
-
+<div class="content-wrapper text-center">
+<h1 class="main_title">Electrical Services</h1>
+      <br>
+      <br>  
+      <p>gjsdfhszhdgfjhzsfb jhsbdfjhsjh sbfhshjf afhvfh hjgewdh bvweV EHGjqh werhwjeh dsfhszbfjh ygfawhrejvbqwN HJWGJHABWM  WHRGUYQWGB jgehjfb wergwehjr bwejhrghwjg bhegrqwy behrhwgb kjrhj </p>
+      <p>gjsdfhszhdgfjhzsfb jhsbdfjhsjh sbfhshjf afhvfh hjgewdh bvweV EHGjqh werhwjeh dsfhszbfjh ygfawhrejvbqwN HJWGJHABWM  WHRGUYQWGB jgehjfb wergwehjr bwejhrghwjg bhegrqwy behrhwgb kjrhj </p>
+      <p>gjsdfhszhdgfjhzsfb jhsbdfjhsjh sbfhshjf afhvfh hjgewdh bvweV EHGjqh werhwjeh dsfhszbfjh ygfawhrejvbqwN HJWGJHABWM  WHRGUYQWGB jgehjfb wergwehjr bwejhrghwjg bhegrqwy behrhwgb kjrhj </p>
+      <p>gjsdfhszhdgfjhzsfb jhsbdfjhsjh sbfhshjf afhvfh hjgewdh bvweV EHGjqh werhwjeh dsfhszbfjh ygfawhrejvbqwN HJWGJHABWM  WHRGUYQWGB jgehjfb wergwehjr bwejhrghwjg bhegrqwy behrhwgb kjrhj </p>
+      <p>gjsdfhszhdgfjhzsfb jhsbdfjhsjh sbfhshjf afhvfh hjgewdh bvweV EHGjqh werhwjeh dsfhszbfjh ygfawhrejvbqwN HJWGJHABWM  WHRGUYQWGB jgehjfb wergwehjr bwejhrghwjg bhegrqwy behrhwgb kjrhj </p>
+      <p>gjsdfhszhdgfjhzsfb jhsbdfjhsjh sbfhshjf afhvfh hjgewdh bvweV EHGjqh werhwjeh dsfhszbfjh ygfawhrejvbqwN HJWGJHABWM  WHRGUYQWGB jgehjfb wergwehjr bwejhrghwjg bhegrqwy behrhwgb kjrhj </p>
+      <p>gjsdfhszhdgfjhzsfb jhsbdfjhsjh sbfhshjf afhvfh hjgewdh bvweV EHGjqh werhwjeh dsfhszbfjh ygfawhrejvbqwN HJWGJHABWM  WHRGUYQWGB jgehjfb wergwehjr bwejhrghwjg bhegrqwy behrhwgb kjrhj </p>
+      <p>gjsdfhszhdgfjhzsfb jhsbdfjhsjh sbfhshjf afhvfh hjgewdh bvweV EHGjqh werhwjeh dsfhszbfjh ygfawhrejvbqwN HJWGJHABWM  WHRGUYQWGB jgehjfb wergwehjr bwejhrghwjg bhegrqwy behrhwgb kjrhj </p>
+</div>
 
 @endsection 
--- resources/views/user/services/tilesFittings.blade.php
+++ resources/views/user/services/tilesFittings.blade.php
@@ -1,5 +1,17 @@
 @extends('user/app')
 @section('main-contend')
-
+<div class="content-wrapper text-center">
+<h1 class="main_title">Tiles Fitting</h1>
+      <br>
+      <br>  
+      <p>gjsdfhszhdgfjhzsfb jhsbdfjhsjh sbfhshjf afhvfh hjgewdh bvweV EHGjqh werhwjeh dsfhszbfjh ygfawhrejvbqwN HJWGJHABWM  WHRGUYQWGB jgehjfb wergwehjr bwejhrghwjg bhegrqwy behrhwgb kjrhj </p>
+      <p>gjsdfhszhdgfjhzsfb jhsbdfjhsjh sbfhshjf afhvfh hjgewdh bvweV EHGjqh werhwjeh dsfhszbfjh ygfawhrejvbqwN HJWGJHABWM  WHRGUYQWGB jgehjfb wergwehjr bwejhrghwjg bhegrqwy behrhwgb kjrhj </p>
+      <p>gjsdfhszhdgfjhzsfb jhsbdfjhsjh sbfhshjf afhvfh hjgewdh bvweV EHGjqh werhwjeh dsfhszbfjh ygfawhrejvbqwN HJWGJHABWM  WHRGUYQWGB jgehjfb wergwehjr bwejhrghwjg bhegrqwy behrhwgb kjrhj </p>
+      <p>gjsdfhszhdgfjhzsfb jhsbdfjhsjh sbfhshjf afhvfh hjgewdh bvweV EHGjqh werhwjeh dsfhszbfjh ygfawhrejvbqwN HJWGJHABWM  WHRGUYQWGB jgehjfb wergwehjr bwejhrghwjg bhegrqwy behrhwgb kjrhj </p>
+      <p>gjsdfhszhdgfjhzsfb jhsbdfjhsjh sbfhshjf afhvfh hjgewdh bvweV EHGjqh werhwjeh dsfhszbfjh ygfawhrejvbqwN HJWGJHABWM  WHRGUYQWGB jgehjfb wergwehjr bwejhrghwjg bhegrqwy behrhwgb kjrhj </p>
+      <p>gjsdfhszhdgfjhzsfb jhsbdfjhsjh sbfhshjf afhvfh hjgewdh bvweV EHGjqh werhwjeh dsfhszbfjh ygfawhrejvbqwN HJWGJHABWM  WHRGUYQWGB jgehjfb wergwehjr bwejhrghwjg bhegrqwy behrhwgb kjrhj </p>
+      <p>gjsdfhszhdgfjhzsfb jhsbdfjhsjh sbfhshjf afhvfh hjgewdh bvweV EHGjqh werhwjeh dsfhszbfjh ygfawhrejvbqwN HJWGJHABWM  WHRGUYQWGB jgehjfb wergwehjr bwejhrghwjg bhegrqwy behrhwgb kjrhj </p>
+      <p>gjsdfhszhdgfjhzsfb jhsbdfjhsjh sbfhshjf afhvfh hjgewdh bvweV EHGjqh werhwjeh dsfhszbfjh ygfawhrejvbqwN HJWGJHABWM  WHRGUYQWGB jgehjfb wergwehjr bwejhrghwjg bhegrqwy behrhwgb kjrhj </p>
+</div>
 
 @endsection 
