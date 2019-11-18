f25b272eb2f8a72d32cb5beb102384f8f2e8bffd
rammiahj <rammiahj@gmail.com>
2019-11-18 12:48:01 +0530

    Public User Final 2

--- database/seeds/ProjectTableSeeder.php
+++ database/seeds/ProjectTableSeeder.php
@@ -18,7 +18,28 @@ class ProjectTableSeeder extends Seeder
              'ProAddress' => 'Vanni',
              'description' => '5square area',
              'proStartDate'=>'2019-10-01',
-            ]
+            ],
+            [
+                'ProName' => 'House Building',
+                'ProAddress' => 'Kaithady',
+                'description' => '10sq area',
+                'proStartDate'=>'2019-01-01',
+                'proEndDate' => '2019-05-10',
+            ],
+            [
+                'ProName' => 'Wall Painting',
+                'ProAddress' => 'Avarangal',
+                'description' => '750m',
+                'proStartDate'=>'2019-10-01',
+                'proEndDate' => '2019-11-10',
+            ],
+            [
+                'ProName' => 'House Building',
+                'ProAddress' => 'Chavakachcheri',
+                'description' => '40 sq area',
+                'proStartDate' => '2019-09-10',
+            ],
+            
 
             ];
             foreach($projects as $project){
